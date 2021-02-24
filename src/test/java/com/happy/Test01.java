45f0e01e0d5e971e32e532bbe99ab2732ef48937
zhouzhi <1175974811@qq.com>
2021-02-24 19:09:03 +0800

    添加Test01文件

---
+++ src/test/java/com/happy/Test01.java
@@ -0,0 +1,14 @@
+package com.happy;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Happy
+ * @Date: 2021/02/24/19:08
+ * @Description:
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("cccc");
+    }
+}
